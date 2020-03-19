83ce7d7049c4d607938d50c7192f1711238122df
gpaoloni <paolonigianfranco@gmail.com>
2020-03-19 18:28:45 -0300

    Added JWT security in populateCounselors

--- functions/populateCounselors.ts
+++ functions/populateCounselors.ts
@@ -5,50 +5,51 @@ import {
   ServerlessFunctionSignature,
 } from '@twilio-labs/serverless-runtime-types/types';
 
+const TokenValidator = require('twilio-flex-token-validator').functionValidator;
+
 type EventBody = {
   workspaceSID: string | undefined;
 };
 
-export const handler: ServerlessFunctionSignature = async (
-  context: Context,
-  event: {},
-  callback: ServerlessCallback,
-) => {
-  const response = new Twilio.Response();
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+export const handler: ServerlessFunctionSignature = TokenValidator(
+  async (context: Context, event: {}, callback: ServerlessCallback) => {
+    const response = new Twilio.Response();
+    response.appendHeader('Access-Control-Allow-Origin', '*');
+    response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  try {
-    const body = event as EventBody;
-    const { workspaceSID } = body;
+    try {
+      const body = event as EventBody;
+      console.log(body);
+      const { workspaceSID } = body;
 
-    if (workspaceSID === undefined) {
-      throw new Error('Error: Workspace parameter not provided');
+      if (workspaceSID === undefined) {
+        throw new Error('Error: Workspace parameter not provided');
+      }
+
+      const workspace = await context
+        .getTwilioClient()
+        .taskrouter.workspaces(workspaceSID)
+        .fetch();
+
+      const workers = await workspace.workers().list();
+
+      const prettyWorkers = workers.map(w => ({
+        sid: w.sid,
+        friendlyName: w.friendlyName,
+      }));
+
+      response.setStatusCode(200);
+      response.appendHeader('Content-Type', 'application/json');
+      response.setBody({ prettyWorkers });
+      callback(null, response);
+    } catch (err) {
+      response.setStatusCode(500);
+      response.appendHeader('Content-Type', 'application/json');
+      response.setBody(err);
+      // If there's an error, send an error response
+      // Keep using the response object for CORS purposes
+      callback(null, response);
     }
-
-    const workspace = await context
-      .getTwilioClient()
-      .taskrouter.workspaces(workspaceSID)
-      .fetch();
-
-    const workers = await workspace.workers().list();
-
-    const prettyWorkers = workers.map(w => ({
-      sid: w.sid,
-      friendlyName: w.friendlyName,
-    }));
-
-    response.setStatusCode(200);
-    response.appendHeader('Content-Type', 'application/json');
-    response.setBody({ prettyWorkers });
-    callback(null, response);
-  } catch (err) {
-    response.setStatusCode(500);
-    response.appendHeader('Content-Type', 'application/json');
-    response.setBody(err);
-    // If there's an error, send an error response
-    // Keep using the response object for CORS purposes
-    callback(null, response);
-  }
-};
+  },
+);
