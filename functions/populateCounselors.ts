e153ef7c4bfc824f3922db7f530bf00da0e99086
gpaoloni <paolonigianfranco@gmail.com>
2020-03-20 12:36:21 -0300

    Requested changes

--- functions/populateCounselors.ts
+++ functions/populateCounselors.ts
@@ -9,6 +9,7 @@ const TokenValidator = require('twilio-flex-token-validator').functionValidator;
 
 type EventBody = {
   workspaceSID: string | undefined;
+  helpline: string | undefined;
 };
 
 export const handler: ServerlessFunctionSignature = TokenValidator(
@@ -17,13 +18,18 @@ export const handler: ServerlessFunctionSignature = TokenValidator(
     response.appendHeader('Access-Control-Allow-Origin', '*');
     response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
     response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+    response.appendHeader('Content-Type', 'application/json');
 
     try {
       const body = event as EventBody;
-      const { workspaceSID } = body;
+      const { workspaceSID, helpline } = body;
 
       if (workspaceSID === undefined) {
-        throw new Error('Error: Workspace parameter not provided');
+        const err = { message: 'Error: WorkspaceSID parameter not provided', status: 400 };
+        response.setStatusCode(400);
+        response.setBody(err);
+        callback(null, response);
+        return;
       }
 
       const workspace = await context
@@ -33,18 +39,39 @@ export const handler: ServerlessFunctionSignature = TokenValidator(
 
       const workers = await workspace.workers().list();
 
-      const prettyWorkers = workers.map(w => ({
-        sid: w.sid,
-        friendlyName: w.friendlyName,
-      }));
+      const detailedWorkers = workers.map(w => {
+        const attributes = JSON.parse(w.attributes);
+
+        return {
+          ...w,
+          attributes,
+        };
+      });
+
+      const workerSummaries = detailedWorkers.map(w => {
+        const fullName = w.attributes.full_name;
+        const wHelpline = w.attributes.helpline;
+
+        return {
+          sid: w.sid,
+          fullName,
+          helpline: wHelpline,
+        };
+      });
+
+      if (helpline) {
+        const withFilter = workerSummaries.filter(w => w.helpline === helpline);
+        response.setStatusCode(200);
+        response.setBody({ workerSummaries: withFilter });
+        callback(null, response);
+        return;
+      }
 
       response.setStatusCode(200);
-      response.appendHeader('Content-Type', 'application/json');
-      response.setBody({ prettyWorkers });
+      response.setBody({ workerSummaries });
       callback(null, response);
     } catch (err) {
       response.setStatusCode(500);
-      response.appendHeader('Content-Type', 'application/json');
       response.setBody(err);
       // If there's an error, send an error response
       // Keep using the response object for CORS purposes
