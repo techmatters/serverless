015bea17a9e28620c3a37e3b32bd02d79c368523
murilovmachado <murilovmachado@gmail.com>
2020-05-26 18:14:22 -0300

    Save insights data
    * Set both conversations and customers objects

---
+++ functions/setTaskInsightsData.ts
@@ -0,0 +1,93 @@
+import '@twilio-labs/serverless-runtime-types';
+import {
+  Context,
+  ServerlessCallback,
+  ServerlessFunctionSignature,
+  TwilioResponse,
+} from '@twilio-labs/serverless-runtime-types/types';
+
+const TokenValidator = require('twilio-flex-token-validator').functionValidator;
+
+// TODO: Factor out into lib
+const send = (statusCode: number) => (body: string | object) => (callback: ServerlessCallback) => (
+  response: TwilioResponse,
+) => {
+  response.setStatusCode(statusCode);
+  response.setBody(body);
+  callback(null, response);
+};
+
+// TODO: Factor out into lib
+const responseWithCors = () => {
+  const response = new Twilio.Response();
+
+  response.setHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json',
+  });
+
+  return response;
+};
+
+type Body = {
+  workspaceSID?: string;
+  taskSID?: string;
+  conversations?: string;
+  customers?: string;
+};
+
+export const handler: ServerlessFunctionSignature = TokenValidator(
+  async (context: Context, event: {}, callback: ServerlessCallback) => {
+    const response = responseWithCors();
+
+    try {
+      const body = event as Body;
+      const { workspaceSID, taskSID, conversations, customers } = body;
+
+      if (workspaceSID === undefined) {
+        const err = { message: 'Error: workspaceSID parameter not provided', status: 400 };
+        send(400)(err)(callback)(response);
+        return;
+      }
+
+      if (taskSID === undefined) {
+        const err = { message: 'Error: taskSID parameter not provided', status: 400 };
+        send(400)(err)(callback)(response);
+        return;
+      }
+
+      const taskToBeUpdated = await context
+        .getTwilioClient()
+        .taskrouter.workspaces(workspaceSID)
+        .tasks(taskSID)
+        .fetch();
+
+      const previousAttributes = JSON.parse(taskToBeUpdated.attributes);
+
+      const newAttributes = {
+        ...previousAttributes,
+        conversations: {
+          ...previousAttributes.conversations,
+          ...(conversations && JSON.parse(conversations)),
+          conversation_id: taskSID,
+        },
+        customers: {
+          ...previousAttributes.customers,
+          ...(customers && JSON.parse(customers)),
+        },
+      };
+
+      const updatedTask = await context
+        .getTwilioClient()
+        .taskrouter.workspaces(workspaceSID)
+        .tasks(taskSID)
+        .update({ attributes: JSON.stringify(newAttributes) });
+
+      send(200)(updatedTask)(callback)(response);
+    } catch (err) {
+      send(500)(err)(callback)(response);
+    }
+  },
+);
