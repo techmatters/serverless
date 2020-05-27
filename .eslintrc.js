687c8ebdcb3c2fc24caeb03cd9e3438e09418a4f
gpaoloni <paolonigianfranco@gmail.com>
2020-05-27 19:51:29 -0300

    Forgot to push

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
     ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
     sourceType: 'module',  // Allows for the use of imports
   },
