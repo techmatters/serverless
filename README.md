c0e63bf0611411eea11b9cb03a9d9bb5e2e20f0f
gpaoloni <paolonigianfranco@gmail.com>
2020-06-03 15:21:09 -0300

    Added issuSyncToken + updated README

--- README.md
+++ README.md
@@ -9,15 +9,27 @@ Repository for serverless functions living on the Twilio Serverless Toolkit
 `git clone https://github.com/tech-matters/serverless && cd serverless`  
 2- Install dependencies:  
 `npm install`  
-3- create a .env file with propper ACCOUNT_SID=<account_sid_value> AUTH_TOKEN=<auth_token_value> (can be found inside the twilio console, depending on the enviroment we want to deploy to)  
+3- create a .env file with all the .env variables ([below is the whole list](#environment-variables))  
 4- run typescript compiler (as Twilio ST serves the .js files) and start local server:  
 `npm start`  
 
 For help on twilio-run commands run:  
 `npm run tr -- help`  
    
-To deploy:  
-`npm run tr:deploy`  
+## Environment variables
+| Variable Name                       | Expected Value                                |
+| ----------------------------------- | --------------------------------------------  |
+| `ACCOUNT_SID`                       | sid of the Twilio account                     |
+| `AUTH_TOKEN`                        | auth token of the abouve account              |
+| `TWILIO_WORKSPACE_SID`              | workspace sid for the taskrouter              |
+| `TWILIO_CHAT_TRANSFER_WORKFLOW_SID` | workflow sid within above workspace           |
+| `SYNC_SERVICE_SID`                  | sync service sid for use as temporary storage |
+| `SYNC_SERVICE_API_KEY`              | api resource to use above sync client         |
+| `SYNC_SERVICE_API_SECRET`           | api secret of the above resource              |
+| `CHAT_SERVICE_SID`                  | programmable chat sid used for chat tasks     |
+
+## Deployment
+To deploy (dev environment):  `npm run tr:deploy`  
 [More about deploying](https://www.twilio.com/docs/labs/serverless-toolkit/deploying)
 
 
@@ -33,8 +45,6 @@ Explanation
 "with_uri_params_if_any": append to the uri "param1=<value_1>&param2=<value_2>"  
 "and_valid_Token": finally append to the uri "Token=<valid_token>"  
 
-Token generator util is a work in progress
-
 ## tech-matters-serverless-helpers
 This are helpers and functions reused across the various serverless functions.
 They are packed as npm package because it's the easiest way to reuse the code within a Twilio Serverless Toolkit Project and preserve the typing information TS provides.
@@ -42,6 +52,6 @@ They are packed as npm package because it's the easiest way to reuse the code wi
 It's currenty deployed with Gian's npm account, [contact him](https://github.com/GPaoloni) to deploy new versions!
 
 To deploy:
-once inside the project folder (`cd tech-matters-serverless-helpers`)
-1- `npm run build`
+once inside the project folder (`cd tech-matters-serverless-helpers`)  
+1- `npm run build`  
 2- `npm publish` (must be logged in npm cli)
---
+++ tests/issueSyncToken.test.ts
@@ -0,0 +1,99 @@
+import { ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
+import * as jwt from 'jsonwebtoken';
+import { handler as issueSyncToken, AuthEvent } from '../functions/issueSyncToken';
+
+import helpers, { MockedResponse } from './helpers';
+
+const baseContext = {
+  getTwilioClient: (): any => null,
+  DOMAIN_NAME: 'serverless',
+  ACCOUNT_SID: 'ACxxx',
+  SYNC_SERVICE_API_KEY: 'api-key',
+  SYNC_SERVICE_API_SECRET: 'api-secret',
+  SYNC_SERVICE_SID: 'ISxxx',
+};
+
+describe('issueSyncToken', () => {
+  beforeAll(() => {
+    helpers.setup({});
+  });
+  afterAll(() => {
+    helpers.teardown();
+  });
+
+  test('Should return status 500', async () => {
+    const event1: AuthEvent = {
+      TokenResult: {
+        identity: '',
+      },
+    };
+    const event2: AuthEvent = {
+      TokenResult: {
+        identity: 'worker1',
+      },
+    };
+
+    const callback1: ServerlessCallback = (err, result) => {
+      expect(result).toBeDefined();
+      const response = result as MockedResponse;
+      expect(response.getStatus()).toBe(500);
+      expect(response.getBody().toString()).toContain(
+        'Identity is missing, something is wrong with the token provided',
+      );
+    };
+
+    const callback2: ServerlessCallback = (err, result) => {
+      expect(result).toBeDefined();
+      const response = result as MockedResponse;
+      expect(response.getStatus()).toBe(500);
+      expect(response.getBody().toString()).toContain(
+        'Sync Service information missing, set your env vars',
+      );
+    };
+
+    const anotherContext1 = { ...baseContext, SYNC_SERVICE_SID: undefined };
+    const anotherContext2 = { ...baseContext, SYNC_SERVICE_SID: undefined };
+    const anotherContext3 = { ...baseContext, SYNC_SERVICE_SID: undefined };
+
+    await issueSyncToken(baseContext, event1, callback1);
+    await Promise.all(
+      [anotherContext1, anotherContext2, anotherContext3].map(context =>
+        issueSyncToken(context, event2, callback2),
+      ),
+    );
+  });
+
+  test('Should return status 200 (with valid token)', async () => {
+    const event: AuthEvent = {
+      TokenResult: {
+        identity: 'worker1',
+      },
+    };
+
+    const callback: ServerlessCallback = (err, result) => {
+      expect(result).toBeDefined();
+
+      const response = result as MockedResponse;
+      expect(response.getStatus()).toBe(200);
+
+      const { token } = response.getBody();
+      expect(typeof token).toBe('string');
+
+      const verifyCallback: jwt.VerifyCallback = (error, decoded: any) => {
+        expect(error).toBeNull();
+        expect(decoded.iss).toBe(baseContext.SYNC_SERVICE_API_KEY);
+        expect(decoded.sub).toBe(baseContext.ACCOUNT_SID);
+        expect(decoded.grants).toEqual({
+          identity: event.TokenResult.identity,
+          data_sync: {
+            service_sid: baseContext.SYNC_SERVICE_SID,
+          },
+        });
+      };
+
+      jwt.verify(token, baseContext.SYNC_SERVICE_API_SECRET, verifyCallback);
+    };
+
+    await issueSyncToken(baseContext, event, callback);
+  });
+});
