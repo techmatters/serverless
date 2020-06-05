1cedd0f9604a9a5b75c0a4d0dfa01e295ad7bfaa
gpaoloni <paolonigianfranco@gmail.com>
2020-06-05 13:19:11 -0300

    Added transferCallResolve function

---
+++ functions/transferCallResolve.ts
@@ -0,0 +1,66 @@
+import '@twilio-labs/serverless-runtime-types';
+import {
+  Context,
+  ServerlessCallback,
+  ServerlessFunctionSignature,
+} from '@twilio-labs/serverless-runtime-types/types';
+import {
+  responseWithCors,
+  bindResolve,
+  error400,
+  error500,
+  success,
+} from 'tech-matters-serverless-helpers';
+
+const TokenValidator = require('twilio-flex-token-validator').functionValidator;
+
+type EnvVars = {
+  TWILIO_WORKSPACE_SID: string;
+};
+
+export type Body = {
+  taskSid?: string;
+  reservationSid?: string;
+};
+
+async function closeTask(context: Context<EnvVars>, body: Required<Body>) {
+  const client = context.getTwilioClient();
+
+  const closedReservation = await client.taskrouter
+    .workspaces(context.TWILIO_WORKSPACE_SID)
+    .tasks(body.taskSid)
+    .reservations(body.reservationSid)
+    .update({
+      reservationStatus: 'completed',
+    });
+
+  return closedReservation;
+}
+
+export const handler: ServerlessFunctionSignature = TokenValidator(
+  async (context: Context<EnvVars>, event: Body, callback: ServerlessCallback) => {
+    const response = responseWithCors();
+    const resolve = bindResolve(callback)(response);
+
+    const { taskSid, reservationSid } = event;
+
+    try {
+      if (taskSid === undefined) {
+        resolve(error400('taskSid'));
+        return;
+      }
+      if (reservationSid === undefined) {
+        resolve(error400('reservationSid'));
+        return;
+      }
+
+      const validBody = { taskSid, reservationSid };
+
+      const closedReservation = await closeTask(context, validBody);
+
+      resolve(success({ closed: closedReservation.sid }));
+    } catch (err) {
+      resolve(error500(err));
+    }
+  },
+);
