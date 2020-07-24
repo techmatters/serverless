3ecffda0de9d3c89c70bb148222fccf8e6928f79
Ariel R-A <raarielgrace@gmail.com>
2020-07-24 11:38:18 -0400

    Edited function and added tests.

--- functions/checkBlockList.protected.ts
+++ functions/checkBlockList.protected.ts
@@ -1,16 +1,12 @@
-import {
-  Context,
-  ServerlessCallback,
-  ServerlessFunctionSignature,
-} from '@twilio-labs/serverless-runtime-types/types';
+import { Context, ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
 
-export const handler: ServerlessFunctionSignature = (
-  context: Context,
-  event: any,
-  callback: ServerlessCallback,
-) => {
-  const file = Runtime.getAssets()['/blocklist.json'].open();
-  const blocklist = JSON.parse(file).numbers;
+export interface Event {
+  callFrom: string;
+}
+
+export const handler = (context: Context, event: Event, callback: ServerlessCallback) => {
+  const file = Runtime.getAssets()['/blocklist.json'];
+  const blocklist = file ? JSON.parse(file.open()).numbers : [];
   const blocked = blocklist.some((num: string) => event.callFrom.includes(num));
   if (blocked) {
     throw new Error('User is blocked.');
---
+++ tests/checkBlockList.test.ts
@@ -0,0 +1,67 @@
+import { ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
+import { handler as checkBlockList, Event } from '../functions/checkBlockList.protected';
+
+import helpers from './helpers';
+
+const baseContext = {
+  getTwilioClient: (): any => ({}),
+  DOMAIN_NAME: 'serverless',
+};
+
+describe('checkBlockList defined', () => {
+  beforeAll(() => {
+    const runtime = new helpers.MockRuntime({});
+    // eslint-disable-next-line no-underscore-dangle
+    runtime._addAsset('/blocklist.json', '../assets/blocklist.private.json');
+    helpers.setup({}, runtime);
+  });
+  afterAll(() => {
+    helpers.teardown();
+  });
+
+  test('Should succeed with unblocked caller', () => {
+    const event: Event = { callFrom: '+11234567890' };
+
+    const callback: ServerlessCallback = (err, result) => {
+      expect(result).toBeUndefined();
+      expect(err).toBeNull();
+    };
+
+    checkBlockList(baseContext, event, callback);
+  });
+
+  test('Should error with blocked caller', () => {
+    const event: Event = { callFrom: '+12025550102' }; // fake 555 number
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const callback: ServerlessCallback = (a, b) => {};
+
+    function blocked() {
+      checkBlockList(baseContext, event, callback);
+    }
+
+    expect(blocked).toThrowError(new Error('User is blocked.'));
+  });
+});
+
+describe('checkBlockList undefinied', () => {
+  beforeAll(() => {
+    const runtime = new helpers.MockRuntime({});
+    // eslint-disable-next-line no-underscore-dangle
+    runtime._missingAsset('/blocklist.json');
+    helpers.setup({}, runtime);
+  });
+  afterAll(() => {
+    helpers.teardown();
+  });
+
+  test('Should succeed with no blocklist', () => {
+    const event: Event = { callFrom: '+11234567890' };
+
+    const callback: ServerlessCallback = (err, result) => {
+      expect(result).toBeUndefined();
+      expect(err).toBeNull();
+    };
+
+    checkBlockList(baseContext, event, callback);
+  });
+});
