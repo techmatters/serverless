3665e612b277da717618ce331f621ba75bedcdbd
raarielgrace <raarielgrace@gmail.com>
2020-08-27 16:54:16 -0400

    Added chatbot redirection function and tests. (#20)
    * Added chatbot redirection function and tests.
    
    * Bug fix for autopilot behavior
    
    * Bug fix.

---
+++ functions/autopilotRedirect.protected.ts
@@ -0,0 +1,40 @@
+import { Context, ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
+import { JsonObject } from 'swagger-ui-express';
+
+export interface Event {
+  Memory: string;
+}
+
+export const handler = (context: Context, event: Event, callback: ServerlessCallback) => {
+  const memory = JSON.parse(event.Memory);
+  const returnObj: JsonObject = { actions: [] };
+
+  switch (memory.at) {
+    case 'survey':
+    case 'gender_why':
+      // eslint-disable-next-line no-case-declarations
+      const { gender } = memory.twilio.collected_data.collect_survey.answers;
+
+      // Handle someone asking "why" or questioning what is meant by gender
+      // Answers to the "why" question will never produce this
+      if (gender.error === undefined && gender.answer.toLowerCase() === 'why') {
+        returnObj.actions.push({
+          redirect: 'task://gender_why',
+        });
+        break;
+      }
+
+      returnObj.actions.push({
+        redirect: 'task://counselor_handoff',
+      });
+      break;
+    default:
+      // If we ever get here, it's in error
+      // Just handoff to counselor for now, maybe need to internally record an error
+      returnObj.actions.push({
+        redirect: 'task://counselor_handoff',
+      });
+      break;
+  }
+  callback(null, returnObj);
+};
