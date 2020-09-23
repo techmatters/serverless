f918fc398e7cba2e5e895158a132ac1c5cf7e416
gpaoloni <paolonigianfranco@gmail.com>
2020-09-23 14:37:00 -0300

    Reworked redirect

--- functions/autopilotRedirect.protected.ts
+++ functions/autopilotRedirect.protected.ts
@@ -1,40 +1,57 @@
 import { Context, ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
-import { JsonObject } from 'swagger-ui-express';
 
 export interface Event {
   Memory: string;
+  Channel: string;
 }
 
-export const handler = (context: Context, event: Event, callback: ServerlessCallback) => {
-  const memory = JSON.parse(event.Memory);
-  const returnObj: JsonObject = { actions: [] };
+type EnvVars = {
+  TWILIO_CHAT_TRANSFER_WORKFLOW_SID: string;
+};
 
+const buildActionsArray = (context: Context<EnvVars>, event: Event) => {
+  const memory = JSON.parse(event.Memory);
   switch (memory.at) {
-    case 'survey':
-    case 'gender_why':
-      // eslint-disable-next-line no-case-declarations
+    case 'survey': {
+      if (event.Channel === 'voice') {
+        const say = { say: 'Hold on, we are connecting you with an agent' };
+        const handoff = {
+          handoff: {
+            channel: 'voice',
+            uri: `taskrouter://${context.TWILIO_CHAT_TRANSFER_WORKFLOW_SID}`,
+          },
+        };
+        return [say, handoff];
+      }
+
+      const redirect = { redirect: 'task://counselor_handoff' };
+      return [redirect];
+    }
+    case 'gender_why': {
       const { gender } = memory.twilio.collected_data.collect_survey.answers;
 
       // Handle someone asking "why" or questioning what is meant by gender
       // Answers to the "why" question will never produce this
       if (gender.error === undefined && gender.answer.toLowerCase() === 'why') {
-        returnObj.actions.push({
-          redirect: 'task://gender_why',
-        });
-        break;
+        const redirect = { redirect: 'task://gender_why' };
+        return [redirect];
       }
 
-      returnObj.actions.push({
-        redirect: 'task://counselor_handoff',
-      });
-      break;
-    default:
+      const redirect = { redirect: 'task://counselor_handoff' };
+      return [redirect];
+    }
+    default: {
       // If we ever get here, it's in error
       // Just handoff to counselor for now, maybe need to internally record an error
-      returnObj.actions.push({
-        redirect: 'task://counselor_handoff',
-      });
-      break;
+      const redirect = { redirect: 'task://counselor_handoff' };
+      return [redirect];
+    }
   }
+};
+
+export const handler = (context: Context<EnvVars>, event: Event, callback: ServerlessCallback) => {
+  const actions = buildActionsArray(context, event);
+  const returnObj = { actions };
+
   callback(null, returnObj);
 };
