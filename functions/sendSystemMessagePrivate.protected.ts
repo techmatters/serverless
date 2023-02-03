d989bdc89d51be6011b59c11f69f9eff917eda83
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-02-03 10:30:19 -0300

    Added log

--- functions/sendSystemMessagePrivate.protected.ts
+++ functions/sendSystemMessagePrivate.protected.ts
@@ -2,6 +2,7 @@ import { Context, ServerlessCallback } from '@twilio-labs/serverless-runtime-typ
 import { Body, EnvVars, SendSystemMessageModule } from './sendSystemMessage';
 
 export const handler = (context: Context<EnvVars>, event: Body, callback: ServerlessCallback) => {
+  console.log(' -------------------- sendSystemMessagePrivate.protected --------------------');
   const handlerPath = Runtime.getFunctions().sendSystemMessage.path;
   // eslint-disable-next-line import/no-dynamic-require, global-require
   const { sendSystemMessage } = require(handlerPath) as SendSystemMessageModule;
