0c99980331e35a82c41819580c94ec681672a12f
Gianfranco Paoloni <paolonigianfranco@gmail.com>
2023-07-14 18:20:27 -0300

    Modified helpers to support chatbot capture refactor

--- functions/helpers/hrmDataManipulation.private.ts
+++ functions/helpers/hrmDataManipulation.private.ts
@@ -16,10 +16,13 @@
 
 import { get } from 'lodash';
 // eslint-disable-next-line prettier/prettier
-import type { BotMemory } from '../postSurveyComplete.protected';
+import type { AutopilotMemory } from '../postSurveyComplete.protected';
+import type { LexMemory } from './lexClient.private';
 import type { OneToManyConfigSpec } from './insightsService.private';
 
 export type PostSurveyData = { [question: string]: string | number };
+
+type BotMemory = AutopilotMemory | LexMemory;
 
 /**
  * Given a bot's memory returns a function to reduce over an array of OneToManyConfigSpec.
@@ -27,11 +30,12 @@ export type PostSurveyData = { [question: string]: string | number };
  * and return a flattened object of type PostSurveyData
  */
 const flattenOneToMany =
-  (memory: BotMemory) => (accum: PostSurveyData, curr: OneToManyConfigSpec) => {
+  (memory: BotMemory, pathBuilder: (question: string) => string) =>
+  (accum: PostSurveyData, curr: OneToManyConfigSpec) => {
     const paths = curr.questions.map(
       (question) => ({
         question,
-        path: `twilio.collected_data.collect_survey.answers.${question}.answer`,
+        path: pathBuilder(question),
       }), // Path where the answer for each question should be in bot memory
     );
 
@@ -46,8 +50,12 @@ const flattenOneToMany =
 /**
  * Given the config for the post survey and the bot's memory, returns the collected answers in the fomat it's stored in HRM.
  */
-export const buildDataObject = (oneToManyConfigSpecs: OneToManyConfigSpec[], memory: BotMemory) => {
-  const reducerFunction = flattenOneToMany(memory);
+export const buildDataObject = (
+  oneToManyConfigSpecs: OneToManyConfigSpec[],
+  memory: BotMemory,
+  pathBuilder: (question: string) => string = (q) => q,
+) => {
+  const reducerFunction = flattenOneToMany(memory, pathBuilder);
   return oneToManyConfigSpecs.reduce<PostSurveyData>(reducerFunction, {});
 };
 
