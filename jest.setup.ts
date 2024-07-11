742068e0f1461eaf3b7bd3ad2e171e964b407f90
Stephen Okpalaononuju <stephenokpala@gmail.com>
2024-07-11 15:45:41 +0100

    ch: fix jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 import fetchMock from 'jest-fetch-mock';
+
 fetchMock.enableMocks();
